60b404b015fe2c26168897c4602fe86c1c6b78fc
mohamaddmortada <mortada_mohamad@hotmail.com>
2024-11-10 23:02:59 +0200

    adding bookmark and footer

---
+++ front/js/footer.js
@@ -0,0 +1,22 @@
+let homePage=document.getElementById('home-page');
+let moviesList=document.getElementById('movies-list');
+let signIn=document.getElementById('sign-in');
+let bookMark=document.getElementById('book-mark');
+let chatBot=document.getElementById('chat-bot');
+
+homePage.addEventListener('click',()=>{
+    window.location.href="home-page.html"
+});
+
+moviesList.addEventListener('click',()=>{
+    window.location.href="movies-page.html"
+});
+signIn.addEventListener('click',()=>{
+    window.location.href="login.html"
+});
+chatBot.addEventListener('click',()=>{
+    window.location.href="chatbot.html"
+});
+bookMark.addEventListener('click',()=>{
+    window.location.href="bookmark.html"
+});
