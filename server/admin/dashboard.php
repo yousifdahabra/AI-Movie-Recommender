2dbe1376ad543aa6a3155f9edde252a31420d551
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-11-12 02:51:20 +0200

    Add Dashboard - PHP - update user status on database

--- server/admin/dashboard.php
+++ server/admin/dashboard.php
@@ -29,3 +29,35 @@ if(isset($_POST['get_users'])){
 
 }
 
+if(isset($_POST['update_status_users'])){
+    if(isset($_POST['user_id'])){
+        $user_id = $_POST['user_id'];
+        $users_query = $conection->prepare("Select * from users_tbl where user_id  = ?   ");
+        $users_query->bind_param("i",$user_id);
+        $users_query->execute();
+        $get_users = $users_query->get_result();
+        $user = $get_users->fetch_assoc();
+        $is_active = $user['is_active'];
+        if($is_active == 0){
+            $users_update_query = $conection->prepare("update users_tbl set is_active = 1 where user_id  = ?   ");
+            $users_update_query->bind_param("i",$user_id);
+            $users_update_query->execute();
+        }else{
+            $users_update_query = $conection->prepare("update users_tbl set is_active = 0 where user_id  = ?   ");
+            $users_update_query->bind_param("i",$user_id);
+            $users_update_query->execute();
+        }
+        $response = [
+            "states" => "1",
+            "result" => "Update user status",
+        ];
+        echo  json_encode($response)  ;
+
+    }else{
+        $response = [
+            "states" => "0",
+            "result" => "There are no Users",
+        ];
+        echo  json_encode($response)  ;
+    }
+}
