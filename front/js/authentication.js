40c94c8670c4810b830bbb09523c6cba1ceb4a41
Salah Haddara <Salahhaddara02@gmail.com>
2024-11-13 16:19:26 +0200

    connecting the authentication login to the api

--- front/js/authentication.js
+++ front/js/authentication.js
@@ -1,3 +1,43 @@
+const loginForm = document.getElementById('loginForm');
+const signupForm = document.getElementById('signupForm');
+
+loginForm.addEventListener('submit', handleLogin);
+
+async function handleLogin(event) {
+    event.preventDefault();
+
+    const email = document.getElementById('loginEmail').value;
+    const password = document.getElementById('loginPassword').value;
+
+    try {
+        const response = await fetch('http://localhost/AI-Movie-Recommender/server/authentication/login.php', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({username: email, password})
+        });
+
+        const data = await response.json();
+
+        if (data.success) {
+            localStorage.setItem('user', JSON.stringify(data.user));
+            window.location.href = '/dashboard';
+        } else {
+            showError('loginEmailError', data.message);
+            showError('loginPasswordError', data.message);
+        }
+    } catch (error) {
+        console.error('Error logging in:', error);
+        showError('loginEmailError', 'An error occurred. Please try again later.');
+        showError('loginPasswordError', 'An error occurred. Please try again later.');
+    }
+}
+
+function showError(id, message) {
+    document.getElementById(id).textContent = message;
+}
+
 function toggleForms() {
     const loginForm = document.querySelector('.login-form');
     const signupForm = document.querySelector('.signup-form');
