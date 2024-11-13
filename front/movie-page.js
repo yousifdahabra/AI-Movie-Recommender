c4302847ac9d2b97293735065e165cba56476138
mohamaddmortada <mortada_mohamad@hotmail.com>
2024-11-14 00:02:47 +0200

    adding bookmark icon to the movie section

--- front/movie-page.js
+++ front/movie-page.js
@@ -4,3 +4,10 @@ document.getElementById('movie-title').innerText = selected.movie_title;
 document.getElementById('movie-summary').innerText = selected.movie_summary;
 document.getElementById('movie-release-date').innerText = selected.movie_release_date;
 
+let addBookmark=document.getElementById('add-bookmark');
+addBookmark.addEventListener('click',()=>{
+    addBookmarktoDB()
+})
+function addBookmarktoDB(){
+
+}
