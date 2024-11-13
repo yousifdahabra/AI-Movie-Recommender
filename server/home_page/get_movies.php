98c5f784a10a74cf534cd4b149af7cdd72bfee29
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-11-14 01:11:30 +0200

    Add Home Page - PHP - Add get_movies API for home page sections

--- server/home_page/get_movies.php
+++ server/home_page/get_movies.php
@@ -1,7 +1,27 @@
 <?php 
 include "../conection.php";
 
+$mvoies_query = $conection->prepare("Select * from movies_tbl");
+$mvoies_query->execute();
+$get_movies = $mvoies_query->get_result();
+$movies_array = [];
+if($get_movies->num_rows > 0){
+    while($movies = $get_movies->fetch_assoc()){
+        $movies_array[] = $movies;
+    }
+    $response = [
+        "states" => "1",
+        "result" => $movies_array,
+    ];
+    echo json_encode($response);
 
+}else{
+    $response = [
+        "states" => "0",
+        "result" => "There are no movies",
+    ];
+    echo  json_encode($response)  ;
+}
 
 
 ?>
