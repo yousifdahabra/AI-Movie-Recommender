e8e6d03d1b3e7948dc2ec08211bd0d87713b150d
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-11-14 06:14:28 +0200

    Add Home Page - PHP/SQL - add a query to get user rate and give the user recomand movies base on his rate will get the recomand on other hand if user not login will see other user recomand each time will see diffrent 4

--- server/home_page/get_movies.php
+++ server/home_page/get_movies.php
@@ -8,6 +8,44 @@ if(isset($_GET['filter_by'])){
     }else if($_GET['filter_by'] == 'trend'){
         $mvoies_query = $conection->prepare("SELECT m.*,sum(scale) as total_scale FROM movies_tbl m INNER JOIN movies_rate_tbl mr on mr.movie_id = m.movie_id GROUP BY m.movie_id order by total_scale DESC limit 4");
 
+    }else if($_GET['filter_by'] == 'recommend'){
+        if (isset($_SESSION['user_id'])) {
+            $user_id=$_SESSION['user_id'];
+            $mvoies_query = $conection->prepare("SELECT m.movie_id
+                FROM movies_tbl m
+                JOIN movies_category_tbl mc ON m.movie_id = mc.movie_id
+                WHERE mc.category_id IN (
+                    SELECT DISTINCT mc2.category_id
+                    FROM movies_category_tbl mc2
+                    JOIN movies_rate_tbl r ON mc2.movie_id = r.movie_id
+                    WHERE r.user_id = ?
+                )
+                AND m.movie_id NOT IN (
+                    SELECT r.movie_id
+                    FROM movies_rate_tbl r
+                    WHERE r.user_id = ?
+                )
+                limit 4;");
+            $mvoies_query->bind_param('ii',$user_id,$user_id);
+
+        }else{
+            $mvoies_query = $conection->prepare("
+                SELECT m.*
+                FROM movies_tbl m
+                JOIN movies_category_tbl mc ON m.movie_id = mc.movie_id
+                WHERE mc.category_id IN (
+                    SELECT DISTINCT mc2.category_id
+                    FROM movies_category_tbl mc2
+                    JOIN movies_rate_tbl r ON mc2.movie_id = r.movie_id
+                )
+                AND m.movie_id NOT IN (
+                    SELECT r.movie_id
+                    FROM movies_rate_tbl r
+                )
+                ORDER BY RAND()
+                LIMIT 4;
+            ");
+        }
     }else{
         $mvoies_query = $conection->prepare("Select * from movies_tbl  limit 4");
     }
