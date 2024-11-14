9f0f9230203ce6a2e7f0b51a6d170b7eaf37fd59
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-11-14 02:28:31 +0200

    Add Home Page - PHP/SQL - Implement query to display trending movies based on user ratings, showing highest-rated movies first

--- server/home_page/get_movies.php
+++ server/home_page/get_movies.php
@@ -5,6 +5,9 @@ if(isset($_GET['filter_by'])){
     if($_GET['filter_by'] == 'new'){
         $mvoies_query = $conection->prepare("Select * from movies_tbl order by movie_release_date DESC limit 4");
 
+    }else if($_GET['filter_by'] == 'trend'){
+        $mvoies_query = $conection->prepare("SELECT m.*,sum(scale) as total_scale FROM movies_tbl m INNER JOIN movies_rate_tbl mr on mr.movie_id = m.movie_id GROUP BY m.movie_id order by total_scale DESC limit 4");
+
     }else{
         $mvoies_query = $conection->prepare("Select * from movies_tbl  limit 4");
     }
